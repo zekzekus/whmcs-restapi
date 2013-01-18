bd476be2b4bfedda3a9ec00bcf944607b6251e8e
Zekeriya Koc <zekeriyak@ttn.com.tr>
2013-01-18 16:04:29 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+import sys
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+NEWS = open(os.path.join(here, 'NEWS.txt')).read()
+
+
+version = '0.0.1'
+
+install_requires = [
+    'flask',
+    'pywhmcs',
+]
+
+
+setup(
+    name='whmcs-restapi',
+    version=version,
+    description="Rest API server for WHMCS SOAP like HTTP API",
+    long_description=README + '\n\n' + NEWS,
+    classifiers=[
+
+    ],
+    keywords='rest server api flask whmcs http',
+    author='Zekeriya Koc',
+    author_email='zekzekus@gmail.com',
+    url='',
+    license='Gnu General Public License v3',
+    packages=find_packages('src'),
+    package_dir={'': 'src'}, include_package_data=True,
+    zip_safe=False,
+    install_requires=install_requires,
+    entry_points={
+        'console_scripts':
+            ['whmcs-restapi=whmcsrestapi:main']
+    }
+)
